712e487ed09b48236c45160c556e0531b1c6a90e
Kirk Lund <klund@apache.org>
2016-04-28 15:51:58 -0700

    Revert back to UnitTest and file new Jira ticket for these tests.

--- extensions/geode-modules-tomcat7/src/test/java/com/gemstone/gemfire/modules/session/Tomcat7SessionsJUnitTest.java
+++ extensions/geode-modules-tomcat7/src/test/java/com/gemstone/gemfire/modules/session/Tomcat7SessionsJUnitTest.java
@@ -17,11 +17,11 @@
 package com.gemstone.gemfire.modules.session;
 
 import com.gemstone.gemfire.modules.session.catalina.Tomcat7DeltaSessionManager;
-import com.gemstone.gemfire.test.junit.categories.IntegrationTest;
+import com.gemstone.gemfire.test.junit.categories.UnitTest;
 import org.junit.BeforeClass;
 import org.junit.experimental.categories.Category;
 
-@Category(IntegrationTest.class)
+@Category(UnitTest.class)
 public class Tomcat7SessionsJUnitTest extends TestSessionsBase {
 
   // Set up the session manager we need
